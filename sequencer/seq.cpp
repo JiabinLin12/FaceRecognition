b28eaabaa4d9a7699007c654687f3ae35c845916
chth2844 <chirayu.thakur@colorado.edu>
2021-04-26 20:19:51 -0600

    added sequencer

---
+++ sequencer/seq.cpp
@@ -0,0 +1,504 @@
+/* ========================================================================== */
+/*                                                                            */
+// Sam Siewert, December 2017
+//
+// Sequencer Generic
+//
+// The purpose of this code is to provide an example for how to best
+// sequence a set of periodic services for problems similar to and including
+// the final project in real-time systems.
+//
+// For example: Service_1 for camera frame aquisition
+//              Service_2 for image analysis and timestamping
+//              Service_3 for image processing (difference images)
+//              Service_4 for save time-stamped image to file service
+//              Service_5 for save processed image to file service
+//              Service_6 for send image to remote server to save copy
+//              Service_7 for elapsed time in syslog each minute for debug
+//
+// At least two of the services need to be real-time and need to run on a single
+// core or run without affinity on the SMP cores available to the Linux 
+// scheduler as a group.  All services can be real-time, but you could choose
+// to make just the first 2 real-time and the others best effort.
+//
+// For the standard project, to time-stamp images at the 1 Hz rate with unique
+// clock images (unique second hand / seconds) per image, you might use the 
+// following rates for each service:
+//
+// Sequencer - 30 Hz 
+//                   [gives semaphores to all other services]
+// Service_1 - 3 Hz  , every 10th Sequencer loop
+//                   [buffers 3 images per second]
+// Service_2 - 1 Hz  , every 30th Sequencer loop 
+//                   [time-stamp middle sample image with cvPutText or header]
+// Service_3 - 0.5 Hz, every 60th Sequencer loop
+//                   [difference current and previous time stamped images]
+// Service_4 - 1 Hz, every 30th Sequencer loop
+//                   [save time stamped image with cvSaveImage or write()]
+// Service_5 - 0.5 Hz, every 60th Sequencer loop
+//                   [save difference image with cvSaveImage or write()]
+// Service_6 - 1 Hz, every 30th Sequencer loop
+//                   [write current time-stamped image to TCP socket server]
+// Service_7 - 0.1 Hz, every 300th Sequencer loop
+//                   [syslog the time for debug]
+//
+// With the above, priorities by RM policy would be:
+//
+// Sequencer = RT_MAX	@ 30 Hz
+// Servcie_1 = RT_MAX-1	@ 3 Hz
+// Service_2 = RT_MAX-2	@ 1 Hz
+// Service_3 = RT_MAX-3	@ 0.5 Hz
+// Service_4 = RT_MAX-2	@ 1 Hz
+// Service_5 = RT_MAX-3	@ 0.5 Hz
+// Service_6 = RT_MAX-2	@ 1 Hz
+// Service_7 = RT_MIN	0.1 Hz
+//
+// Here are a few hardware/platform configuration settings on your Jetson
+// that you should also check before running this code:
+//
+// 1) Check to ensure all your CPU cores on in an online state.
+//
+// 2) Check /sys/devices/system/cpu or do lscpu.
+//
+//    Tegra is normally configured to hot-plug CPU cores, so to make all
+//    available, as root do:
+//
+//    echo 0 > /sys/devices/system/cpu/cpuquiet/tegra_cpuquiet/enable
+//    echo 1 > /sys/devices/system/cpu/cpu1/online
+//    echo 1 > /sys/devices/system/cpu/cpu2/online
+//    echo 1 > /sys/devices/system/cpu/cpu3/online
+//
+// 3) Check for precision time resolution and support with cat /proc/timer_list
+//
+// 4) Ideally all printf calls should be eliminated as they can interfere with
+//    timing.  They should be replaced with an in-memory event logger or at
+//    least calls to syslog.
+//
+// 5) For simplicity, you can just allow Linux to dynamically load balance
+//    threads to CPU cores (not set affinity) and as long as you have more
+//    threads than you have cores, this is still an over-subscribed system
+//    where RM policy is required over the set of cores.
+
+// This is necessary for CPU affinity macros in Linux
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <unistd.h>
+
+#include <pthread.h>
+#include <sched.h>
+#include <time.h>
+#include <semaphore.h>
+
+#include <syslog.h>
+#include <sys/time.h>
+#include <sys/sysinfo.h>
+
+#include <errno.h>
+#include <iostream>
+
+#define NSEC_PER_SEC (1000000000)
+#define USEC_PER_MSEC (1000)
+#define NANOSEC_PER_SEC (1000000000)
+#define NSEC_PER_MICROSEC (1000)
+#define NSEC_PER_MSEC (1000000)
+#define NUM_CPU_CORES (1)
+#define TRUE (1)
+#define FALSE (0)
+
+#define NUM_THREADS (2+1)
+
+int abortTest=FALSE;
+int abortS1=FALSE, abortS2=FALSE;
+sem_t semS1, semS2;
+struct timeval start_time_val;
+
+typedef struct
+{
+    int threadIdx;
+    unsigned long long sequencePeriods;
+} threadParams_t;
+
+struct timespec start_time = {0, 0};
+struct timespec finish_time = {0, 0};
+struct timespec thread_dt = {0, 0};
+
+struct timespec temp = {0, 0};
+
+struct timespec time_1 = {0, 0};
+struct timespec time_2 = {0, 0};
+
+void *Sequencer(void *threadp);
+
+void *Service_1(void *threadp);
+void *Service_2(void *threadp);
+double getTimeMsec(void);
+void print_scheduler(void);
+
+using namespace std;
+
+int delta_t(struct timespec *stop, struct timespec *start, struct timespec *delta_t)
+{
+  int dt_sec=stop->tv_sec - start->tv_sec;
+  int dt_nsec=stop->tv_nsec - start->tv_nsec;
+
+  if(dt_sec >= 0)
+  {
+    if(dt_nsec >= 0)
+    {
+      delta_t->tv_sec=dt_sec;
+      delta_t->tv_nsec=dt_nsec;
+    }
+    else
+    {
+      delta_t->tv_sec=dt_sec-1;
+      delta_t->tv_nsec=NSEC_PER_SEC+dt_nsec;
+    }
+  }
+  else
+  {
+    if(dt_nsec >= 0)
+    {
+      delta_t->tv_sec=dt_sec;
+      delta_t->tv_nsec=dt_nsec;
+    }
+    else
+    {
+      delta_t->tv_sec=dt_sec-1;
+      delta_t->tv_nsec=NSEC_PER_SEC+dt_nsec;
+    }
+  }
+
+  return(1);
+}
+
+int main( int argc, char** argv )
+{
+    struct timeval current_time_val;
+    int i, rc, scope;
+    cpu_set_t threadcpu;
+    pthread_t threads[NUM_THREADS];
+    threadParams_t threadParams[NUM_THREADS];
+    pthread_attr_t rt_sched_attr[NUM_THREADS];
+    int rt_max_prio, rt_min_prio;
+    struct sched_param rt_param[NUM_THREADS];
+    struct sched_param main_param;
+    pthread_attr_t main_attr;
+    pid_t mainpid;
+    cpu_set_t allcpuset;
+
+    printf("Starting Sequencer Demo\n");
+    gettimeofday(&start_time_val, (struct timezone *)0);
+    gettimeofday(&current_time_val, (struct timezone *)0);
+    syslog(LOG_CRIT, "Sequencer @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+
+   printf("System has %d processors configured and %d available.\n", get_nprocs_conf(), get_nprocs());
+
+   CPU_ZERO(&allcpuset);
+
+   for(i=0; i < NUM_CPU_CORES; i++)
+       CPU_SET(i, &allcpuset);
+
+   printf("Using CPUS=%d from total available.\n", CPU_COUNT(&allcpuset));
+
+
+    // initialize the sequencer semaphores
+    //
+    if (sem_init (&semS1, 0, 0)) { printf ("Failed to initialize S1 semaphore\n"); exit (-1); }
+    if (sem_init (&semS2, 0, 0)) { printf ("Failed to initialize S2 semaphore\n"); exit (-1); }
+    
+
+    mainpid=getpid();
+
+    rt_max_prio = sched_get_priority_max(SCHED_FIFO);
+    rt_min_prio = sched_get_priority_min(SCHED_FIFO);
+
+    rc=sched_getparam(mainpid, &main_param);
+    main_param.sched_priority=rt_max_prio;
+    rc=sched_setscheduler(getpid(), SCHED_FIFO, &main_param);
+    if(rc < 0) perror("main_param");
+    print_scheduler();
+
+
+    pthread_attr_getscope(&main_attr, &scope);
+
+    if(scope == PTHREAD_SCOPE_SYSTEM)
+      printf("PTHREAD SCOPE SYSTEM\n");
+    else if (scope == PTHREAD_SCOPE_PROCESS)
+      printf("PTHREAD SCOPE PROCESS\n");
+    else
+      printf("PTHREAD SCOPE UNKNOWN\n");
+
+    printf("rt_max_prio=%d\n", rt_max_prio);
+    printf("rt_min_prio=%d\n", rt_min_prio);
+
+    for(i=0; i < NUM_THREADS; i++)
+    {
+
+      CPU_ZERO(&threadcpu);
+      CPU_SET(3, &threadcpu);
+
+      rc=pthread_attr_init(&rt_sched_attr[i]);
+      rc=pthread_attr_setinheritsched(&rt_sched_attr[i], PTHREAD_EXPLICIT_SCHED);
+      rc=pthread_attr_setschedpolicy(&rt_sched_attr[i], SCHED_FIFO);
+      //rc=pthread_attr_setaffinity_np(&rt_sched_attr[i], sizeof(cpu_set_t), &threadcpu);
+
+      rt_param[i].sched_priority=rt_max_prio-i;
+      pthread_attr_setschedparam(&rt_sched_attr[i], &rt_param[i]);
+
+      threadParams[i].threadIdx=i;
+    }
+   
+    printf("Service threads will run on %d CPU cores\n", CPU_COUNT(&threadcpu));
+
+    // Create Service threads which will block awaiting release for:
+    //
+
+    // Servcie_1 = RT_MAX-1	@ 3 Hz
+    //
+    rt_param[1].sched_priority=rt_max_prio-1;
+    pthread_attr_setschedparam(&rt_sched_attr[1], &rt_param[1]);
+    rc=pthread_create(&threads[1],               // pointer to thread descriptor
+                      &rt_sched_attr[1],         // use specific attributes
+                      //(void *)0,               // default attributes
+                      Service_1,                 // thread function entry point
+                      (void *)&(threadParams[1]) // parameters to pass in
+                     );
+    if(rc < 0)
+        perror("pthread_create for service 1");
+    else{
+        printf("pthread_create successful for service 1\n");
+    }
+        
+
+
+    // Service_2 = RT_MAX-2	@ 1 Hz
+    //
+    rt_param[2].sched_priority=rt_max_prio-2;
+    pthread_attr_setschedparam(&rt_sched_attr[2], &rt_param[2]);
+    rc=pthread_create(&threads[2], &rt_sched_attr[2], Service_2, (void *)&(threadParams[2]));
+    if(rc < 0)
+        perror("pthread_create for service 2");
+    else{
+        printf("pthread_create successful for service 2\n");
+    }
+        
+
+
+    // Wait for service threads to initialize and await release by sequencer.
+    //
+    // Note that the sleep is not necessary of RT service threads are created wtih 
+    // correct POSIX SCHED_FIFO priorities compared to non-RT priority of this main
+    // program.
+    //
+    // usleep(1000000);
+ 
+    // Create Sequencer thread, which like a cyclic executive, is highest prio
+    printf("Start sequencer\n");
+    threadParams[0].sequencePeriods=900;
+
+    // Sequencer = RT_MAX	@ 30 Hz
+    //
+    rt_param[0].sched_priority=rt_max_prio;
+    pthread_attr_setschedparam(&rt_sched_attr[0], &rt_param[0]);
+    rc=pthread_create(&threads[0], &rt_sched_attr[0], Sequencer, (void *)&(threadParams[0]));
+    if(rc < 0)
+        perror("pthread_create for sequencer service 0");
+    else
+        printf("pthread_create successful for sequeencer service 0\n");
+
+
+   for(i=0;i<NUM_THREADS;i++)
+       pthread_join(threads[i], NULL);
+
+   printf("\nTEST COMPLETE\n");
+   printf("Thread idx=1 ,Worst Case Execution Time timestamp %ld sec, %ld msec (%ld microsec), ((%ld nanosec))\n\n", time_1.tv_sec, (time_1.tv_nsec / NSEC_PER_MSEC), (time_1.tv_nsec / NSEC_PER_MICROSEC), time_1.tv_nsec);
+   printf("Thread idx=2 ,Worst Case Execution Time timestamp %ld sec, %ld msec (%ld microsec), ((%ld nanosec))\n\n", time_2.tv_sec, (time_2.tv_nsec / NSEC_PER_MSEC), (time_2.tv_nsec / NSEC_PER_MICROSEC), time_2.tv_nsec);
+  
+}
+
+
+void *Sequencer(void *threadp)
+{
+    struct timeval current_time_val;
+    struct timespec delay_time = {0,16666666}; // delay for 16.66 msec, 60 Hz
+    struct timespec remaining_time;
+    double current_time;
+    double residual;
+    int rc, delay_cnt=0;
+    unsigned long long seqCnt=0;
+    threadParams_t *threadParams = (threadParams_t *)threadp;
+
+    gettimeofday(&current_time_val, (struct timezone *)0);
+    syslog(LOG_CRIT, "Sequencer thread @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+    printf("Sequencer thread @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+
+    do
+    {
+        delay_cnt=0; residual=0.0;
+
+        //gettimeofday(&current_time_val, (struct timezone *)0);
+        //syslog(LOG_CRIT, "Sequencer thread prior to delay @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+        do
+        {
+            rc=nanosleep(&delay_time, &remaining_time);
+
+            if(rc == EINTR)
+            { 
+                residual = remaining_time.tv_sec + ((double)remaining_time.tv_nsec / (double)NANOSEC_PER_SEC);
+
+                if(residual > 0.0) printf("residual=%lf, sec=%d, nsec=%d\n", residual, (int)remaining_time.tv_sec, (int)remaining_time.tv_nsec);
+ 
+                delay_cnt++;
+            }
+            else if(rc < 0)
+            {
+                perror("Sequencer nanosleep");
+                exit(-1);
+            }
+           
+        } while((residual > 0.0) && (delay_cnt < 100));
+
+        seqCnt++;
+        gettimeofday(&current_time_val, (struct timezone *)0);
+        syslog(LOG_CRIT, "Sequencer cycle %llu @ sec=%d, msec=%d\n", seqCnt, (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+
+
+        if(delay_cnt > 1) printf("Sequencer looping delay %d\n", delay_cnt);
+
+
+        // Release each service at a sub-rate of the generic sequencer rate
+
+        // Servcie_1 = RT_MAX-1	@ 30 Hz
+        //printf("start time service 1 @ sec=%d, msec=%f\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (double)current_time_val.tv_usec/USEC_PER_MSEC);
+        if((seqCnt % 2) == 0) sem_post(&semS1);
+        //printf("end time service 1 @ sec=%d, msec=%f\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (double)current_time_val.tv_usec/USEC_PER_MSEC);
+        
+
+        // Service_2 = RT_MAX-2	@ 1 Hz
+        //printf("start time service 2 @ sec=%d, msec=%f\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (double)current_time_val.tv_usec/USEC_PER_MSEC);
+        if((seqCnt % 60) == 0) sem_post(&semS2);
+        //printf("end time service 2 @ sec=%d, msec=%f\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (double)current_time_val.tv_usec/USEC_PER_MSEC);
+
+        //gettimeofday(&current_time_val, (struct timezone *)0);
+        //syslog(LOG_CRIT, "Sequencer release all sub-services @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+
+    } while(!abortTest && (seqCnt < threadParams->sequencePeriods));
+
+    sem_post(&semS1); sem_post(&semS2); 
+    abortS1=TRUE; abortS2=TRUE; 
+
+    pthread_exit((void *)0);
+}
+
+
+
+void *Service_1(void *threadp)
+{
+    struct timeval current_time_val;
+    double current_time;
+    unsigned long long S1Cnt=0;
+    threadParams_t *threadParams = (threadParams_t *)threadp;
+
+    gettimeofday(&current_time_val, (struct timezone *)0);
+    syslog(LOG_CRIT, "Frame Sampler thread @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+    printf("Frame Sampler thread @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+
+    while(!abortS1)
+    {
+        sem_wait(&semS1);
+        clock_gettime(CLOCK_REALTIME, &start_time);
+        S1Cnt++;
+
+        gettimeofday(&current_time_val, (struct timezone *)0);
+        syslog(LOG_CRIT, "Frame Sampler release %llu @ sec=%d, msec=%d\n", S1Cnt, (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+        clock_gettime(CLOCK_REALTIME, &finish_time);
+
+        delta_t(&finish_time, &start_time, &thread_dt);
+        
+        printf("Thread idx=1 Count = %lld ,timestamp %ld sec, %ld msec (%ld microsec), ((%ld nanosec))\n\n",  S1Cnt, thread_dt.tv_sec, (thread_dt.tv_nsec / NSEC_PER_MSEC), (thread_dt.tv_nsec / NSEC_PER_MICROSEC),thread_dt.tv_nsec);
+        
+        temp = thread_dt;
+        delta_t(&temp, &time_1, &thread_dt);
+
+        if((thread_dt.tv_sec)>=0)
+        {
+            time_1 = temp;
+        }
+    }
+    pthread_exit((void *)0);
+}
+
+
+void *Service_2(void *threadp)
+{
+    struct timeval current_time_val;
+    double current_time;
+    unsigned long long S2Cnt=0;
+    threadParams_t *threadParams = (threadParams_t *)threadp;
+
+    gettimeofday(&current_time_val, (struct timezone *)0);
+    syslog(LOG_CRIT, "Time-stamp with Image Analysis thread @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+    printf("Time-stamp with Image Analysis thread @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+
+    while(!abortS2)
+    {
+        sem_wait(&semS2);
+        clock_gettime(CLOCK_REALTIME, &start_time);
+        S2Cnt++;
+
+        gettimeofday(&current_time_val, (struct timezone *)0);
+        syslog(LOG_CRIT, "Time-stamp with Image Analysis release %llu @ sec=%d, msec=%d\n", S2Cnt, (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
+    
+        clock_gettime(CLOCK_REALTIME, &finish_time);
+
+        delta_t(&finish_time, &start_time, &thread_dt);
+        
+        printf("Thread idx=2 Count = %lld ,timestamp %ld sec, %ld msec (%ld microsec), ((%ld nanosec))\n\n",  S2Cnt, thread_dt.tv_sec, (thread_dt.tv_nsec / NSEC_PER_MSEC), (thread_dt.tv_nsec / NSEC_PER_MICROSEC),thread_dt.tv_nsec);
+        
+        temp = thread_dt;
+        delta_t(&temp, &time_2, &thread_dt);
+
+        if((thread_dt.tv_sec)>=0)
+        {
+            time_2 = temp;
+        }
+    }
+
+    pthread_exit((void *)0);
+}
+
+
+double getTimeMsec(void)
+{
+  struct timespec event_ts = {0, 0};
+
+  clock_gettime(CLOCK_MONOTONIC, &event_ts);
+  return ((event_ts.tv_sec)*1000.0) + ((event_ts.tv_nsec)/1000000.0);
+}
+
+
+void print_scheduler(void)
+{
+   int schedType;
+
+   schedType = sched_getscheduler(getpid());
+
+   switch(schedType)
+   {
+       case SCHED_FIFO:
+           printf("Pthread Policy is SCHED_FIFO\n");
+           break;
+       case SCHED_OTHER:
+           printf("Pthread Policy is SCHED_OTHER\n"); 
+         break;
+       case SCHED_RR:
+           printf("Pthread Policy is SCHED_RR\n"); exit(-1);
+           break;
+       default:
+           printf("Pthread Policy is UNKNOWN\n"); exit(-1);
+   }
+}
+
